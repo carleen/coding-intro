266ba119bd2c85c1296ba45b1e59d835897bfcf4
carleen <carleenboyer@gmail.com>
2017-11-18 15:06:47 -0500

    final staircase solved

--- staircase.py
+++ staircase.py
@@ -16,7 +16,11 @@ string = ''
 print("Enter a number:")
 n = int(input().strip())
 
+for i in range(1, n+1):
+    string = string + ' '
+
 
 for i in range(1, n+1):
+    string = string[1:]
     string = string + '#'
     print(string)
