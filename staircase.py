4786feb07374d14c2a96ddb32f17cea803abdd21
carleen <carleenboyer@gmail.com>
2017-11-18 15:10:45 -0500

    includes comments

--- staircase.py
+++ staircase.py
@@ -10,16 +10,18 @@ n = 5
 
 import sys
 
-#global variables
+#global variable
 string = ''
 
+#Asks user for input, and converts to integer
 print("Enter a number:")
 n = int(input().strip())
 
+#Creates an initial string of spaces of n length
 for i in range(1, n+1):
     string = string + ' '
 
-
+#Removes the first space from the string, adds a # to the end
 for i in range(1, n+1):
     string = string[1:]
     string = string + '#'
